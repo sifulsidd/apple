a9116cd0a8ee259add8b8c87b53775d2c8efabbc
sifulsidd <sifulsiddiki@yahoo.com>
2025-04-25 21:05:41 -0400

    Got the page and initial things up and loaded

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { appleImg, searchImg, bagImg } from '../utils'
+import { navLists } from '../constants'
+const Navbar = () =>{
+    return (
+        <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
+            <nav className='flex w-full screen-max-width'>
+                <img src={appleImg} alt='Apple' width={14} height={18}/>
+                <div className='flex flex-1 justify-center max-sm:hidden'>
+                {/* makes a new title for each of the items on the page */}
+                {/* takes the navlist from constants and adds it to the page */}
+                {navLists.map((nav, i) => (
+                    <div key={i} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>{nav}</div>))}
+                </div>
+
+                <div>
+                    <img src={searchImg} alt="search"  width={18} height={18}/>
+                    <img src={bagImg} alt="bag"  width={18} height={18}/>
+                </div>
+                
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
